cc6525fe39dfcdac2b1c933a148171468c0a0043
Fernando Munguia Moreno <fer_fcb99@outlook.com>
2022-04-29 16:54:22 -0500

    Se sube ejemplo de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './dashboard/create/create.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReadComponent } from './dashboard/read/read.component';
+import { UpdateComponent } from './dashboard/update/update.component';
 
-const routes: Routes = [];
+// localhost:4200/create
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'create', component: CreateComponent, children: [
+      {
+        path: 'read', component: ReadComponent
+      }, 
+      {
+        path: 'update', component: UpdateComponent
+      }
+    ]
+  },
+  { path: 'read', component: ReadComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateComponent } from './dashboard/create/create.component';
+import { ReadComponent } from './dashboard/read/read.component';
+import { UpdateComponent } from './dashboard/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
